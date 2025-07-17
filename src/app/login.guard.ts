b6830bf3c71586687ed14e8f01aed5edd10e589c
Po Chen <chenpaul914@gmail.com>
2025-07-17 13:09:37 +0000

    ng 16

--- src/app/login.guard.ts
+++ src/app/login.guard.ts
@@ -1,19 +1,13 @@
-import {Injectable} from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  Router,
-  RouterStateSnapshot,
-  UrlTree,
-} from '@angular/router';
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 
-import {map, Observable} from 'rxjs';
+import { map, Observable } from 'rxjs';
 
-import {SupaService} from './supa.service';
+import { SupaService } from './supa.service';
 
-@Injectable({providedIn: 'root'})
-export class LoginGuard implements CanActivate {
-  constructor(private supa: SupaService, private router: Router) {}
+@Injectable({ providedIn: 'root' })
+export class LoginGuard {
+  constructor(private supa: SupaService, private router: Router) { }
   canActivate(
     _route: ActivatedRouteSnapshot,
     _state: RouterStateSnapshot,
